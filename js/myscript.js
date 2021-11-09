aa5e6aedf73f66cdb5d0c1514900226842cf7e9f
Amalia <amalialorenaferariu@gmail.com>
2021-11-09 14:22:14 +0100

    passi logici

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+// Descrizione:
+// Visualizzare in pagina 5 numeri casuali.
+    //generare 5 numeri univoci -> ciclo
+    //visualizzarli in pagina 
+// Da lì parte un timer di 30 secondi.
+    //creare il timer di 30 secondi -> setTimeout
+// Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    //fare un ciclo per i 5 prompt 
+    //estrapolare i valori del prompt
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+    //confrontare i numeri dell'utente con i numeri generati dal pc
